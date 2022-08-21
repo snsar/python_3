9a9a674b00ae0988ba77caa80bdd574476a15b45
snsarvn <lthoquyen2002@gmail.com>
2022-08-21 19:09:29 +0700

    ac

---
+++ PY02040.py
@@ -0,0 +1,22 @@
+n = int(input())
+nuaTren = 0
+nuaDuoi = 0
+for i in range(n-1,-1,-1):
+    x = input()
+    ls = list(map(int, x.split()))
+    for j in range(len(ls)):
+        if j < i:
+            nuaTren += ls[j]
+        elif j > i:
+            nuaDuoi += ls[j]
+k = int(input())
+
+chenhLech =  abs(nuaTren - nuaDuoi)
+check = k - chenhLech
+
+if check >= 0:
+    print('YES')
+else:
+    print("NO")
+
+print(chenhLech)
